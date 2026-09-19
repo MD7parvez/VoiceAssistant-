4c7252517e3ecedd94fe2e80c91962f4c46db5a1
MD7parvez <165442469+MD7parvez@users.noreply.github.com>
2026-09-19 14:50:39 +0600

    Bump app to Version 0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.md7parvez.voiceassistant"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
